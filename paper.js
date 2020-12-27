266d1ae93da7c78998ecdbd1fc36961b886c0370
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2020-12-27 10:24:39 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x, y,radius) {
+        var options = {
+            'isStatic': false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2,
+            
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        //this.height = width;
+       // this.width = height;
+       this.radius = radius;
+       this.image=loadImage("paper.png");
+        World.add(world, this.body);
+      } 
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+     
+        ellipse( 0, 0,this.radius,this.radius );
+        pop();
+       
+      }
+}
